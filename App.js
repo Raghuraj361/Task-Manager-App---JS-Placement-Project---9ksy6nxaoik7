7e35c963420bd1fa8e5cd1562ef47211cb46a624
Raghuraj361 <sraghuraj361@gmail.com>
2023-01-06 11:39:28 +0530

    first commit

--- App.js
+++ App.js
@@ -1,1 +1,308 @@
+// regular expression for validation
+const strRegex =  /^[a-zA-Z\s]*$/; // containing only letters
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+/* supports following number formats - (123) 456-7890, (123)456-7890, 123-456-7890, 123.456.7890, 1234567890, +31636363634, 075-63546725 */
+const digitRegex = /^\d+$/;
 
+// -------------------------------------------------- //
+
+const countryList = document.getElementById('country-list');
+const fullscreenDiv = document.getElementById('fullscreen-div');
+const modal = document.getElementById('modal');
+const addBtn = document.getElementById('add-btn');
+const closeBtn = document.getElementById('close-btn');
+const modalBtns = document.getElementById('modal-btns');
+const form = document.getElementById('modal');
+const addrBookList = document.querySelector('#addr-book-list');
+
+// -------------------------------------------------- //
+let addrName = streetAddr = labels = "";
+
+// Address class
+class Address{
+    constructor(id, addrName, streetAddr,labels){
+        this.id = id;
+        this.addrName = addrName;
+        this.streetAddr = streetAddr;
+        this.labels = labels;
+    }
+
+    static getAddresses(){
+        let addresses;
+        if(localStorage.getItem('addresses') == null){
+            addresses = [];
+        } else {
+            addresses = JSON.parse(localStorage.getItem('addresses'));
+        }
+        return addresses;
+    }
+
+    static addAddress(address){
+        const addresses = Address.getAddresses();
+        addresses.push(address);
+        localStorage.setItem('addresses', JSON.stringify(addresses));
+    }
+
+    static deleteAddress(id){
+        const addresses = Address.getAddresses();
+        addresses.forEach((address, index) => {
+            if(address.id == id){
+                addresses.splice(index, 1);
+            }
+        });
+        localStorage.setItem('addresses', JSON.stringify(addresses));
+        form.reset();
+        UI.closeModal();
+        addrBookList.innerHTML = "";
+        UI.showAddressList();
+    }
+
+    static updateAddress(item){
+        const addresses = Address.getAddresses();
+        addresses.forEach(address => {
+            if(address.id == item.id){
+                address.addrName = item.addrName;
+                address.streetAddr = item.streetAddr;
+                address.labels = item.labels;
+            }
+        });
+        localStorage.setItem('addresses', JSON.stringify(addresses));
+        addrBookList.innerHTML = "";
+        UI.showAddressList();
+    }
+}
+
+// UI class
+class UI{
+    static showAddressList(){
+        const addresses = Address.getAddresses();
+        addresses.forEach(address => UI.addToAddressList(address));
+        const notes = document.querySelectorAll('.note');
+        const conts = document.querySelectorAll('#container');
+        let selcon = "start";
+        notes.forEach(ele => {
+                ele.addEventListener('dragstart' , ()=>{
+                    ele.classList.add("drag")
+                })
+                ele.addEventListener('dragend' , ()=>{
+                    location.reload();
+                    ele.classList.remove("drag");
+                    let newadd , newdes;
+                    addresses.forEach(e => {
+                        if(e.id == ele.dataset.id){
+                            newadd = e.addrName;
+                            newdes = e.streetAddr;
+                        }
+                    })
+                     const addressItem = new Address(ele.dataset.id, newadd, newdes , selcon);
+                     Address.updateAddress(addressItem);
+                    
+                })
+                conts.forEach(ele => {
+                    ele.addEventListener('dragover' , () => {
+                        const tarCon = document.querySelector('.drag');
+                        ele.appendChild(tarCon);
+                        selcon = ele.className;
+                    })
+            })
+        })
+    }
+
+    static addToAddressList(address){
+        const tableRow = document.createElement('div');
+        tableRow.setAttribute('data-id', address.id);
+        tableRow.innerHTML = `
+        <div id="note-wrapper">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dot" viewBox="0 0 16 16">
+            <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+        </svg>
+        <div class="note" id="note" data-id=${address.id} draggable="true" > 
+        <p><span class = "addressing-name">${address.addrName}</span></p>
+       </div>
+       </div>
+        `;
+        if(address.labels === "start"){
+            document.querySelector('.start').appendChild(tableRow);
+        }else if(address.labels === "process"){
+            document.querySelector('.process').appendChild(tableRow);
+        }else if(address.labels === "reviwe"){
+            document.querySelector('.reviwe').appendChild(tableRow);
+        }else{
+            document.querySelector('.done').appendChild(tableRow);
+        }
+      
+
+    }
+
+    static showModalData(id){
+        const addresses = Address.getAddresses();
+        addresses.forEach(address => {
+            if(address.id == id){
+                form.addr_ing_name.value = address.addrName;
+                form.street_addr.value = address.streetAddr;
+                form.labels.value = address.labels;
+                document.getElementById('modal-title').innerHTML = "Change Address Details";
+
+                document.getElementById('modal-btns').innerHTML = `
+                    <button type = "submit" id = "update-btn" data-id = "${id}">Update </button>
+                    <button type = "button" id = "delete-btn" data-id = "${id}">Delete </button>
+                `;
+            }
+        });
+    }
+
+    static showModal(){
+        modal.style.display = "block";
+        fullscreenDiv.style.display = "block";
+    }
+
+    static closeModal(){
+        modal.style.display = "none";
+        fullscreenDiv.style.display = "none";
+    }
+
+
+
+}
+
+// DOM Content Loaded
+window.addEventListener('DOMContentLoaded', () => {
+    loadJSON(); // loading country list from json file
+    eventListeners();
+    UI.showAddressList();
+});
+
+// event listeners
+function eventListeners(){
+    // show add item modal
+    addBtn.addEventListener('click', () => {
+        form.reset();
+        document.getElementById('modal-title').innerHTML = "Add Address";
+        UI.showModal();
+        document.getElementById('modal-btns').innerHTML = `
+            <button type = "submit" id = "save-btn"> Save </button>
+        `;
+    });
+
+    // close add item modal
+    closeBtn.addEventListener('click', UI.closeModal);
+
+    // add an address item
+    modalBtns.addEventListener('click', (event) => {
+        event.preventDefault();
+        if(event.target.id == "save-btn"){
+            let isFormValid = getFormData();
+            if(!isFormValid){
+                form.querySelectorAll('input').forEach(input => {
+                    setTimeout(() => {
+                        input.classList.remove('errorMsg');
+                    }, 1500);
+                });
+            } else {
+                let allItem = Address.getAddresses();
+                let lastItemId = (allItem.length > 0) ? allItem[allItem.length - 1].id : 0;
+                lastItemId++;
+
+                const addressItem = new Address(lastItemId, addrName, streetAddr,labels);
+                Address.addAddress(addressItem);
+                UI.closeModal();
+                UI.addToAddressList(addressItem);
+                form.reset();
+            }
+        }
+    });
+
+    // table row items
+    addrBookList.addEventListener('click', (event) => {
+        UI.showModal();
+        let trElement;
+        if(event.target.parentElement.tagName == "P"){
+            trElement = event.target.parentElement.parentElement;
+        }
+
+        if(event.target.parentElement.tagName == "DIV"){
+            trElement = event.target.parentElement;
+        }
+        let viewID = trElement.dataset.id;
+        UI.showModalData(viewID);
+    });
+
+    // delete an address item
+    modalBtns.addEventListener('click', (event) => {
+        if(event.target.id == 'delete-btn'){
+            Address.deleteAddress(event.target.dataset.id);
+        }
+    });
+
+    // update an address item
+    modalBtns.addEventListener('click', (event) => {
+        event.preventDefault();
+        location.reload();
+        if(event.target.id == "update-btn"){
+            let id = event.target.dataset.id;
+            let isFormValid = getFormData();
+            if(!isFormValid){
+                form.querySelectorAll('input').forEach(input => {
+                    setTimeout(() => {
+                        input.classList.remove('errorMsg');
+                    }, 1500);
+                });
+            } else {
+                const addressItem = new Address(id, addrName, streetAddr, labels);
+                Address.updateAddress(addressItem);
+                UI.closeModal();
+                form.reset();
+            }
+        }
+        
+    });
+}
+
+
+// load countries list
+function loadJSON(){
+    fetch('countries.json')
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        data.forEach(country => {
+            html += `
+                <option> ${country.country} </option>
+            `;
+        });
+    })
+}
+
+// get form data
+function getFormData(){
+    let inputValidStatus = [];
+    // console.log(form.addr_ing_name.value, form.first_name.value, form.last_name.value, form.email.value, form.phone.value, form.street_addr.value, form.postal_code.value, form.city.value, form.country.value, form.labels.value);
+
+    if(!strRegex.test(form.addr_ing_name.value) || form.addr_ing_name.value.trim().length == 0){
+        addErrMsg(form.addr_ing_name);
+        inputValidStatus[0] = false;
+    } else {
+        addrName = form.addr_ing_name.value;
+        inputValidStatus[0] = true;
+    }
+
+   
+
+    if(!(form.street_addr.value.trim().length > 0)){
+        addErrMsg(form.street_addr);
+        inputValidStatus[1] = false;
+    } else {
+        streetAddr = form.street_addr.value;
+        inputValidStatus[1] = true;
+    }
+
+  
+    labels = form.labels.value;
+    return inputValidStatus.includes(false) ? false : true;
+}
+
+
+function addErrMsg(inputBox){
+    inputBox.classList.add('errorMsg');
+}
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# html-css-js-project-boilerplate
+# Task Manager
+
+<img src="https://camo.githubusercontent.com/87f42bf875777602b58116da80b52d14b0023ff1808a707542fa7d6afb7b690f/687474703a2f2f7265732e636c6f7564696e6172792e636f6d2f646d3768376538786a2f696d6167652f75706c6f61642f76313530343330393732332f7461736b2d6d616e616765722d76616e696c6c612d6a735f786e39686e722e706e67" />
+
+### What and Why this project?
+NewtonSchool JS Projects - You can Create, Edit and Delete Notes. The data's are saved on LocalStorage using JavaScript.
+
+> AND ALL THESE ARE WORKING WITHOUT A DATABASE.
+## Basic Lang Info:
+This Project is developed using HTML, CSS, JS.
+
+## Release Notes
+
+Version 0.1.0
+
+* Initial release
+
+## Features:
+1. User can add List.
+2. User can add cards inside list.
+3. User allowed to edit card information.
+4. User data is saved using LocalStorage.
+5. user is allowed to edit board name.
+6. Drag and Drop from one state to another
+7. Developed using Plain Javascript, HTML & CSS.
+
+## Author
+** Sharad Sharma
