649f8a3ddc0092d3c071a6d42627d26f2e347b5a
RAGHURAJ SINGH RAJPOOT <114226990+Raghuraj361@users.noreply.github.com>
2023-01-07 12:51:06 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,50 +6,42 @@ const modle_container = document.querySelector('.modle-container');
 
 input.setAttribute("placeholder", "Please Enter Task");
 
-
+button.addEventListener('click', addTask)
 function addTask() {
     if(input.value === "") {
         alert("please enter task");
     }
     else {
     let inputValue = input.value;
-
+    // i create a card div 
     const openList = document.createElement('div');
     openList.classList = "container-list1-item";
+    // i create a name 
     const nameOfTask = document.createElement('h4');
     nameOfTask.innerText = inputValue;
+    // i create a card description(paragraph)
     const descriptionOfTask = document.createElement('p');
     descriptionOfTask.classList = "description";
-    // const desDiv = document.createElement('div');
-    // desDiv.classList = "desc-div";
-    // desDiv.appendChild(descriptionOfTask);
-
+    // i create a card delete btn
     const deleteTask = document.createElement('button');
     deleteTask.classList = "delete";
     deleteTask.innerText = "";
+    // i create a bar line 
     const itemColor = document.createElement('div');
     itemColor.classList.add('item-color');
     openList.appendChild(itemColor);
-    // const descBtn = document.createElement('div');
-    // descBtn.classList.add('desc-btn');
-    // // add paragraph using modal
-    // descriptionOfTask.appendChild(descBtn);
-    // descBtn.addEventListener('click', function () {
-    //     openList.remove();
-    // });
-
-
-
+    
     openList.append(nameOfTask, descriptionOfTask, deleteTask)
     containerList.appendChild(openList);
 
+    // I have created a function to remove the card
     deleteTask.addEventListener('click', (e) => {
         e.stopPropagation()
         openList.remove();
     })
     editModel(openList, nameOfTask, descriptionOfTask);
     input.value = null;
-
+    // drag & drop functionality
     openList.setAttribute("draggable", "true");
     openList.addEventListener('dragstart', () => {
         openList.classList.add('dragging');
@@ -57,7 +49,6 @@ function addTask() {
     openList.addEventListener('dragend', () => {
         openList.classList.remove('dragging');
     });
-
     const listcontainer = document.querySelectorAll('.container-list');
     listcontainer.forEach((list) => {
         list.addEventListener('dragover', (e) => {
@@ -66,41 +57,45 @@ function addTask() {
             list.appendChild(draggingElm);
         })
     })
-
 }
 }
 
+// i create a modal functionality
 function editModel(div, ip, des) {
     div.addEventListener('dblclick', (e) => {
         e.stopPropagation()
+
+        // store div
         const storeDiv = document.createElement('div');
         storeDiv.classList = "modle-container-style"
+        // input lable
         const taskLable = document.createElement('lable');
         taskLable.innerText = "Task Name";
+        // description lable
         const descriptionLable = document.createElement('lable');
         descriptionLable.innerText = "Description";
-
+        // i create a modal input
         const inputTask = document.createElement('input');
         inputTask.classList = "editInput";
         inputTask.setAttribute('id', 'edit1');
         inputTask.setAttribute('type', 'text');
         inputTask.value = ip.innerText;
-
+        // i create a modal textarea
         const textArea = document.createElement('textarea');
         textArea.classList = "editInput"
         textArea.setAttribute('cols', '5');
         textArea.setAttribute('rows', '5');
         textArea.value = des.innerText;
-
+        // i create a save, clese btn div
         const divButton = document.createElement('div');
         divButton.classList = "buttonDiv";
-
+        // i create a save btn
         const saveBtn = document.createElement('button');
         saveBtn.classList = "saveButton"
         saveBtn.innerText = 'save';
-
+        // i create a close btn
         const deleteBtn = document.createElement('button');
-        deleteBtn.classList = "deleteButton"
+        deleteBtn.classList = "closeButton"
         deleteBtn.innerText = 'close';
 
         divButton.appendChild(saveBtn);
@@ -113,7 +108,7 @@ function editModel(div, ip, des) {
         console.log(div.childNodes);
     })
 }
-
+ 
 function saveTheEditedValue(btnFun, child, des, mainDiv, input1, input2, condition) {
     btnFun.addEventListener('click', (e) => {
         e.stopPropagation();
@@ -127,7 +122,3 @@ function saveTheEditedValue(btnFun, child, des, mainDiv, input1, input2, conditi
 
     })
 }
-   
-
-button.addEventListener('click', addTask)
-
